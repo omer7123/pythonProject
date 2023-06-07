5daa19e749c12c2dd484e7a6b66d0ce28d26a84c
omer7123 <ilyafomin125@gmail.com>
2023-06-07 23:58:33 +0700

    final

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from typing import List
-
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
+
 from api import router
 
 app = FastAPI()
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",port=25565, reload=True)
+    uvicorn.run("main:app", reload=True)
